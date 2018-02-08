0474d13c16cd2cdf9cac12cf7f7b6041969ae7b1
Senthilvel Samatharman <Senthilvel_Samatharman@symantec.com>
2018-02-08 22:39:01 +0530

    Add a naive recursive solution for coin change problem..

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CoinChange
+{
+    class Program
+    {
+        public static long MakeChange(int[] coins, int money, int index)
+        {
+            if(money == 0){
+                return 1;
+            }
+
+            if(index >= coins.Length){
+                return 0;
+            }
+
+            int amountWithCoin = 0;
+            long ways = 0;
+            while(amountWithCoin <= money)
+            {
+                int remaining = money - amountWithCoin;                
+                amountWithCoin += coins[index];
+
+                ways += MakeChange(coins, remaining, index + 1);
+            }
+            return ways;
+        }
+        static void Main(string[] args)
+        {
+            const int Amount = 5;
+            int[] denominations = { 1,2,5,10 };
+            Console.WriteLine(MakeChange(denominations,5,0));
+            
+        }
+    }
+}
